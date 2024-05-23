20b6ad911cb668c053ea3b5f4eb44dadd63dea46
carlos Rincones <carlosrincones3@gmail.com>
2024-05-23 15:39:13 -0400

    prueba git ver commentario linea 10

---
+++ barras.cs
@@ -0,0 +1,297 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace carl
+{
+    class barras
+    {
+        public static async Task dosbarras()
+        {
+          Console.Out.Flush();
+             //////////izquierda camara
+                            SetCursorPosition(2,2);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar modificado para git
+
+                            Console.Out.Flush();
+
+                            SetCursorPosition(2,3);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(2,4);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+
+                             Console.Out.Flush();
+
+                            SetCursorPosition(2,5);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+
+                            Console.Out.Flush();
+
+                            SetCursorPosition(2,6);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(2,7);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(2,8);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(2,9);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(2,10);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(2,11);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(1,12);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("─┤");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(2,13);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(2,14);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                               SetCursorPosition(2,15);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(2,16);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(2,17);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(2,18);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(2,19);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                           SetCursorPosition(2,20);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                           SetCursorPosition(2,21);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                           SetCursorPosition(2,22);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            //barras derecha
+                            SetCursorPosition(77,2);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(77,3);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(77,4);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            
+
+                            SetCursorPosition(77,5);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(77,6);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(77,7);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(77,8);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(77,9);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(77,10);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(77,11);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(77,12);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("├─");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(77,13);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(77,14);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                               SetCursorPosition(77,15);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(77,16);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(77,17);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(77,18);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(77,19);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                           SetCursorPosition(77,20);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                           SetCursorPosition(77,21);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                           SetCursorPosition(77,22);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("│");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+
+                            SetCursorPosition(36,2);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("───┬───");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                            SetCursorPosition(36,22);
+                           ForegroundColor= ConsoleColor.White;
+                              BackgroundColor= ConsoleColor.DarkCyan;
+                           Write("───┴───");//para que no se corra el programa al maximizar
+                            Console.Out.Flush();
+
+                          
+
+                          
+                           await Task.Delay(35);
+                            Console.Out.Flush();
+
+        }
+    }
+}
+
